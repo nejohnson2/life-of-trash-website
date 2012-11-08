612966f4e3741a2fc43e77faa3b6c6d1d01247bb
nejohnson2 <nejohnson2@gmail.com>
2012-11-08 18:35:30 -0500

    twilio

--- web.js
+++ web.js
@@ -2,6 +2,42 @@ var sys = require('sys');
 var express = require('express'),
 	app = express.createServer();
 
+/*********** SERVER CONFIGURATION *****************/
+app.configure(function() {
+    
+    /*********************************************************************************
+        Configure the template engine
+        We will use EJS (Embedded JavaScript) https://github.com/visionmedia/ejs
+        
+        Using templates keeps your logic and code separate from your HTML.
+        We will render the html templates as needed by passing in the necessary data.
+    *********************************************************************************/
+    app.set('port', process.env.PORT || 3000);
+    app.set('view engine','ejs');  // use the EJS node module
+    app.set('views',__dirname+ '/views'); // use /views as template directory
+    app.set('view options',{layout:true}); // use /views/layout.html to manage your main header/footer wrapping template
+    app.register('html',require('ejs')); //use .html files in /views
+
+    /******************************************************************
+        The /static folder will hold all css, js and image assets.
+        These files are static meaning they will not be used by
+        NodeJS directly. 
+        
+        In your html template you will reference these assets
+        as yourdomain.heroku.com/img/cats.gif or yourdomain.heroku.com/js/script.js
+    ******************************************************************/
+    app.use(express.static(__dirname + '/static'));
+    
+    //parse any http form post
+    app.use(express.bodyParser());
+    
+    /**** Turn on some debugging tools ****/
+    app.use(express.logger());
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+});
+/*********** END SERVER CONFIGURATION *****************/
+
 app.get('/', function(request, response) {
 	
 	response.send("uh oh, can't find that post");
