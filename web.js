5de2058a22cf09bdf0374cb9834b7c75c4db2df2
nejohnson2 <nejohnson2@gmail.com>
2012-11-08 00:05:08 -0500

    trouble

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var ejs = require('ejs');
-/*
+
 var ACCOUNT_SID = 'ACad716cc4da934be6ad19bf5353312248';
 var AUTH_TOKEN = '3af91684fa2d040f587bf96955cffd82';
 var MY_HOSTNAME = 'dog-simulatro-master';
-
+/*
 var sys = require('sys'),
     TwilioClient = require('twilio').Client,
     client = new TwilioClient(ACCOUNT_SID, AUTH_TOKEN, MY_HOSTNAME);
