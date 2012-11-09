27f2672759e6375a39456618de0e48770106c6ee
nejohnson2 <nejohnson2@gmail.com>
2012-11-08 23:30:01 -0500

    working

--- web.js
+++ web.js
@@ -46,23 +46,44 @@ app.get('/', function(request, response) {
 
 
 app.get('/incoming', function(request, response) {
-		
+	//var message = 
+	
 	response.render("layout.html");
 });
 
 // Create a function to handle our incoming SMS requests (POST request)
 app.post('/incoming', function(req, res) {
   // Extract the From and Body values from the POST data
+/*
   var message = req.body.Body;
   var from = req.body.From;
   sys.log('From: ' + from + ', Message: ' + message);
   
-  var body = req.params.body;
+  var body = req.body.body;
   // Return sender a very nice message
   // twiML to be executed when SMS is received
   var twiml = '<Response><Sms>' + body + '  HA! HA! This response is auto generated.</Sms></Response>';
   
   res.send(twiml, {'Content-Type':'text/xml'}, 200);
+  res.send("are you there?")
+*/
+
+	var message = {
+		"sid": "SM64d837baa9f94353ba603e52ef4a9d25",
+	    "date_created": "Fri, 09 Nov 2012 02:32:10 +0000",
+	    "date_updated": "Fri, 09 Nov 2012 02:32:10 +0000",
+	    "date_sent": null,
+	    "account_sid": "ACad716cc4da934be6ad19bf5353312248",
+	    "to": "+17654307001",
+	    "from": "+16464309130",
+	    "body": "Nick please please please?! I love you <3",
+	    "status": "queued",
+	    "direction": "outbound-api",
+	    "api_version": "2010-04-01",
+	    "price": null,
+	    "uri": "\/2010-04-01\/Accounts\/ACad716cc4da934be6ad19bf5353312248\/SMS\/Messages\/SM64d837baa9f94353ba603e52ef4a9d25.json"		
+	};	
+	res.send(message, {'Content-Type':'text/json'}, 200);
 });
 
 var port = process.env.PORT || 3000;
