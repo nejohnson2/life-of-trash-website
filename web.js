b4242bc882aa36b75eaf5352044e5f7bf6c97a7c
nejohnson2 <nejohnson2@gmail.com>
2013-06-02 15:23:14 -0400

    added two posts -wsf and action carting tour- also fixed css issue

--- web.js
+++ web.js
@@ -33,28 +33,13 @@ var neighborNumber = [];
     
 /*********** SERVER CONFIGURATION *****************/
 app.configure(function() {
-    
-    /*********************************************************************************
-        Configure the template engine
-        We will use EJS (Embedded JavaScript) https://github.com/visionmedia/ejs
-        
-        Using templates keeps your logic and code separate from your HTML.
-        We will render the html templates as needed by passing in the necessary data.
-    *********************************************************************************/
+
     app.set('port', process.env.PORT || 3000);
     app.set('view engine','ejs');  // use the EJS node module
     app.set('views',__dirname+ '/views'); // use /views as template directory
     app.set('view options',{layout:true}); // use /views/layout.html to manage your main header/footer wrapping template
     app.register('html',require('ejs')); //use .html files in /views
 
-    /******************************************************************
-        The /static folder will hold all css, js and image assets.
-        These files are static meaning they will not be used by
-        NodeJS directly. 
-        
-        In your html template you will reference these assets
-        as yourdomain.heroku.com/img/cats.gif or yourdomain.heroku.com/js/script.js
-    ******************************************************************/
     app.use(express.static(__dirname + '/static'));
     //parse any http form post
     app.use(express.bodyParser());
@@ -134,6 +119,10 @@ app.get('/post7', function(request, response) {
 app.get('/post8', function(request, response) {
 
 	response.render('post8.html');
+});
+app.get('/post9', function(request, response) {
+
+	response.render('post9.html');
 });
 
 app.get('/app', function(request, response) {
@@ -267,9 +256,6 @@ app.post('/location', function(req, res) {
   var lon = req.body.lon;
 
   //store values into the location database
-  
-  //
-
 
   res.render('location.html')
 //  res.send("you made it");
@@ -345,146 +331,6 @@ app.post('/sendCarto', function(req, res) {
 	res.send('here');
 
 });
-
-app.get('/neighbor', function(req, res){
-
-
-	res.send('<form method="POST" action="/neighbor">' +
-					'Body: <input type="text" name="Body" />' +	
-					'who is it going to: <input type="text" name="From" />' +	
-					'Twilio Number: <input type="text" name="To" />' +									
-					'<input type="submit" />'+
-					'</form>');	
-  
-/*
-	var query = Neighbor.find({});
-//	query.sort('Date',-1);
-	
-	query.exec({}, function(err, allNeighbors){
-		
-		neighborData = {
-			neighbors : allNeighbors
-		};
-		
-		res.render(neighborData);
-		
-		
-	});
-*/
-});
-
-/*
-app.post('/neighbor', function(req, res){
-	// Setup DB instance
-	var neighbor = new Neighbor();
-	var help = ['EMERGENCIES', 'CHORES/LABOR', 'SOCIALIZING'];
-	
-	//Parse the parameters of the incoming SMS from twilio
-	var body = req.body.Body;
-	var from = req.body.From;
-	var to = req.body.To;
-	var date = req.body.date;
-	
-	var message = "Thanks for registering.";
-	
-	console.log('To : ' + to)
-	console.log('From : ' + from)
-	//Twilio Numbers: Building 1 : 16464612494, Building 2 : 16464612588, Building 3 : 16464612530
-	// Building 4 : 16464309891, Building 5 : 16464025754,
-
-	if( !(neighborNumber.indexOf(from) >= 0))
-		neighborNumber.push(from);
-		
-	console.log(neighborNumber)
-
-	if(to == '+16464612494'){
-		if( body == 'I want to help' || 'yes' ){
-			message = "Thanks for registering with Building 1.  How are you willing to help? A)EMERGENCIES B)CHORES/LABOR C)SOCIAL Choose all that apply.";
-		} else if(body == 'a' || 'A' || 'EMERGENCIES' || 'A)EMERGENCIES' || 'emergencies' || 'a)emergencies') {
-			message = "Thanks for helping with EMERGENCIES.  We will send a text when a neighbor is in need of help or text your needs to this number."; 
-		} else if (body == 'b' || 'B' || 'CHORES/LABOR' || 'B)CHORES/LABOR' || 'chores/labor' || 'b)chores/labor' || 'chores' || 'labor') {
-			message = "Thanks for helping with CHORES/LABOR.  We will send a text when a neighbor is in need of help or text your needs to this number.";
-		} else if (body == 'c' || 'C' || 'SOCIAL' || 'social' || 'C)SOCIAL' || 'c)social'){
-			message = "Thanks for being SOCIAL.  We will send a text when a neighbor is in need or text your needs to this number.";
-		} else { 
-			message = "To register, text 'I want to help.' to 1-646-461-2494";
-		}
-	} else if(to == '+16464612588' ) {
-		message = "Thanks for registering with Building 2.";
-	} else if(to == '+16464612530' ) {
-		message = "Thanks for registering with Building 3.";
-	} else if(to == '+16464309891' ) {
-		message = "Thanks for registering with Building 4.";
-	} else if(to == '+16464025754' ) {
-		message = "Thanks for registering with Building 5.";
-	} else {
-		message = "Thanks for registering.";
-	}
-	console.log(message);
-	// Registration Function:
-	// Start with: 'I want to help"
-	// Thanks for contacting neighbors of ______(Building).  To signu up, reply 'I want to help'.
-	// Thanks for registering.  How are you willing to help? A)EMERGENCIES B)CHORES/LABOR C)SOCIAL? Choose all that apply.
-		// if nothing 
-	// B
-	// Thanks for 'helping with ________'.  We will send a text when a neighbor is in need of help.  If you need help, text this number otherwise we  
-	// We will send a text when a neighbor needs 'help with _______a'.  If you need help, text your needs to 88888. 
-
-
-	//Help Function:
-	//
-	// I need help with ___ at __(time) at __(apartment). 
-	//
-	// We have contacted your neighbors and will update you shortly.
-	//
-	// send sms to people who are registered
-	// 
-	// A neighbor needs help with ______ at _(time)_ at _(location)_.  Text YES if you can help.
-	//
-	// if YES - Thanks for helping your neighbor!
-	// if NO - Thanks.  Maybe next time.
-	//
-	// after a half hour - neighbors are unavailable
-	//	
-	// Send a text to those signed up with 10
-
-	Twilio.SMS.create({to: from, from: to, body: message}, function(err,res) {
-		console.log('Up Up and Away...SMS Sent!');
-	});
-	
-	neighbor.number = from;
-	neighbor.to = to;
-	neighbor.body = body;
-	neighbor.save();
-	
-	res.redirect('/neighbor/' + neighbor.from)
-
-
-/*
-	 // received twilio
-	var from = req.body.From;
-	var to = req.body.To;
-	var body = req.body.Body;
-
-  
-	var newNeighbor = {
-	  from: req.body.To,
-	  body: req.body.Body,
-	  to: 	req.body.From
-	};
-  
-	var Neighbor = new Neighbor(newNeighbor);
-	Neighbor.save();
-	 
-	 
-	var twiml = '<?xml version="1.0" encoding="UTF-8" ?><Response>n<Sms>Thanks for signing up!  What would you like to help with? A) Emergencies, B) Around the House, C) Socializing, D) Pet Walking.</Sms>n</Response>';
-
-    res.send(twiml, {'Content-Type':'text/xml'}, 200);
-*/
-/*
-});
-*/
-
 
 
 var port = process.env.PORT || 3000;
